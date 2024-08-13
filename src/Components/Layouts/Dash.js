bb62a16cb885be39abdcc2e92bd9f8bf9bd9e6a1
Bhanuteja005 <pashikantibhanuteja@gmail.com>
2024-08-13 19:07:11 +0530

    update

--- src/Components/Layouts/Dash.js
+++ src/Components/Layouts/Dash.js
@@ -25,7 +25,18 @@ const Dash = () => {
       </h2>
       <div className="grid grid-cols-1 place-content-center gap-3 px-1 py-4 lg:grid-cols-2 lg:gap-4 lg:px-8 xl:grid-cols-3">
         
-      
+      <Link
+          className="flex gap-2 rounded-lg bg-violet-300 p-6 text-base hover:bg-violet-400/90 dark:bg-violet-950/80 dark:hover:bg-violet-950 dark:hover:text-slate-300 duration-200 lg:text-lg"
+          to={"./paper"}
+        >
+          <GiBookshelf className="text-[2.5rem] lg:text-[4rem] " />
+          <div className="font-semibold">
+            Papers
+            <p className="text-sm font-normal lg:text-base ">
+              View Papers and Notes
+            </p>
+          </div>
+        </Link>
         <Link
           className="flex gap-2 rounded-lg bg-violet-300 p-6 text-base hover:bg-violet-400/90 dark:bg-violet-950/80 dark:hover:bg-violet-950 dark:hover:text-slate-300 duration-200 lg:text-lg"
           to={"./attendance"}
@@ -54,10 +65,21 @@ const Dash = () => {
           </div>
         </Link>
 
-        {user.role === "HOD" && (
+        {user.role === "Principal" && (
           <>
            
-           
+           <Link
+              className="flex gap-2 rounded-lg bg-violet-300 p-6 text-base hover:bg-violet-400/90 dark:bg-violet-950/80 dark:hover:bg-violet-950 dark:hover:text-slate-300 duration-200 lg:text-lg"
+              to={"./add_paper"}
+            >
+              <BiBookAdd className="text-[2.5rem] lg:text-[4rem] " />
+              <div className="font-semibold">
+                Add Paper
+                <p className="text-sm font-normal lg:text-base ">
+                  Add a New Paper
+                </p>
+              </div>
+            </Link>
             <Link
               className="flex gap-2 rounded-lg bg-violet-300 p-6 text-base hover:bg-violet-400/90 dark:bg-violet-950/80 dark:hover:bg-violet-950 dark:hover:text-slate-300 duration-200 lg:text-lg"
               to={"./approve_staff"}
